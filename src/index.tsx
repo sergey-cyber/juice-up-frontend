436bd614247e0355825064eed1c41d7fa535d216
cyber-sergey <zenkovich_so@mail.ru>
2023-06-22 01:26:07 +0800

    homepage added

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/juice_up.ru">
       <App />
     </BrowserRouter>
   </React.StrictMode>
