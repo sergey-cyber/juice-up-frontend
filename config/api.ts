5d07ad425fad5ad558a3a9dc49b08bc6fe328139
cyber-sergey <zenkovich_so@mail.ru>
2023-06-23 21:50:09 +0800

    production api url changed

--- config/api.ts
+++ config/api.ts
@@ -2,5 +2,5 @@ const IP = "92.53.115.123";
 
 export const api_url = {
   development: "http://localhost:8080/api",
-  production: `https://${IP}/juice-up/api`
+  production: `http://${IP}:8099/api`
 };
