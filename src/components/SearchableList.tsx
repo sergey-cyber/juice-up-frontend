b828853e9ebaab9cdae341b512651d9b4a4c671a
cyber-sergey <zenkovich_so@mail.ru>
2023-07-25 00:06:08 +0800

    search filter hid

--- src/components/SearchableList.tsx
+++ src/components/SearchableList.tsx
@@ -1,4 +1,4 @@
-import { Input, List, ListProps, Row } from "antd";
+import { Button, Input, List, ListProps, Row } from "antd";
 import { useEffect, useState } from "react";
 import { Scope } from "../types/entities/Backlog";
 import { Todo } from "../types/entities/Todo";
@@ -11,6 +11,7 @@ export const SearchableList = (props: ListProps<SerachableItem>) => {
   const [filteredTodos, setFilteredTodos] = useState<SerachableItem[]>(
     props.dataSource || []
   );
+  const [showFilter, setShowFilter] = useState(false);
 
   useEffect(
     () => setFilteredTodos(props.dataSource || []),
@@ -21,19 +22,29 @@ export const SearchableList = (props: ListProps<SerachableItem>) => {
   return (
     <List
       header={
-        <Row wrap={false}>
-          <Input
-            onChange={(e) => {
-              const { value } = e.currentTarget;
-              setFilteredTodos(
-                value
-                  ? (props.dataSource || []).filter((at) =>
-                      at.name.toLowerCase().includes(value.toLowerCase())
-                    )
-                  : props.dataSource || []
-              );
-            }}
-            suffix={<SearchOutlined />}
+        <Row wrap={false} justify={"space-between"}>
+          {showFilter ? (
+            <Input
+              placeholder="IInter the name you are looking for..."
+              onChange={(e) => {
+                const { value } = e.currentTarget;
+                setFilteredTodos(
+                  value
+                    ? (props.dataSource || []).filter((at) =>
+                        at.name.toLowerCase().includes(value.toLowerCase())
+                      )
+                    : props.dataSource || []
+                );
+              }}
+            />
+          ) : (
+            <span />
+          )}
+          <Button
+            style={{ marginLeft: 10, width: "36px" }}
+            icon={<SearchOutlined />}
+            onClick={() => setShowFilter(!showFilter)}
+            type={showFilter ? "primary" : "default"}
           />
         </Row>
       }
